3311f0cebf1e76cf5d71a9ba8c190b942d175731
todor-enikov <enigmaenikov@gmail.com>
2016-12-24 21:39:27 +0200

    Filled about component and finished. Also given name on the project. :)

--- Course_Project_Skeleton/app/components/about.component/about.component.ts
+++ Course_Project_Skeleton/app/components/about.component/about.component.ts
@@ -1,8 +1,20 @@
 import { Component } from '@angular/core';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
 
 @Component({
     selector: 'about',
-    templateUrl: './about.component.html'
+    templateUrl: './about.component.html',
+    styles: [
+        `h3 {
+                display: inline-block;
+              }
+        `
+    ]
 })
 
-export class AboutComponent { }
+export class AboutComponent {
+    authors: FirebaseListObservable<any[]>;
+    constructor(af: AngularFire) {
+        this.authors = af.database.list('authors');
+    }
+}
