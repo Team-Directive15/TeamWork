6d0305da6d29c3a70636614968c189d27ee5beb5
TalicZealot <talic.zealot@gmail.com>
2016-12-25 21:15:30 +0200

    more placeholders

--- Course_Project_Skeleton/app/components/product.components/products.component.ts
+++ Course_Project_Skeleton/app/components/product.components/products.component.ts
@@ -1,9 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../../models/product.model';
 
 @Component({
-    selector: 'products',
+    selector: 'productsList',
     templateUrl: './products.component.html'
 })
 
-export class ProductsComponent {
+export class ProductsComponent implements OnInit{
+    products: Product[];
+
+    ngOnInit() {
+        this.products = [new Product('placeholder', 'placeholder'), new Product('placeholder2', 'placeholder2')];
+    }
 }
