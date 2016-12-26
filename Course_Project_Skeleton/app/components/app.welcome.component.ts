d78a938c409485d282152cf7ef1df07439b8a8ab
todor-enikov <enigmaenikov@gmail.com>
2016-12-26 22:38:42 +0200

    Finished home page component.

--- Course_Project_Skeleton/app/components/app.welcome.component.ts
+++ Course_Project_Skeleton/app/components/app.welcome.component.ts
@@ -2,7 +2,23 @@ import { Component } from '@angular/core'
 
 @Component({
     selector: 'app-content-loading',
-    templateUrl: './app.welcome.component.html'
+    templateUrl: './app.welcome.component.html',
+    styles:[`
+    p {
+        width: 380px;
+        display: inline;
+    }
+    img {
+        display: block;
+        margin-left: auto;
+        margin-right: auto;
+        margin-top: 20px;
+        margin-bottom: 20px;    
+        max-width: 100%;
+        width: auto\9;    
+        width: 800px;
+    }
+    `]
 })
 
 export class WelcomeComponent { }
