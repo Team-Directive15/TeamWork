beb142ef4c14c0f3334b062fbbf1ecd492f74d4c
TalicZealot <talic.zealot@gmail.com>
2016-12-29 17:08:56 +0200

    reviews can link to product

--- Course_Project_Skeleton/app/models/review.model.ts
+++ Course_Project_Skeleton/app/models/review.model.ts
@@ -4,12 +4,16 @@ export class Review {
     rating: Number;
     text: string;
     expert: boolean;
+    productName: string;
+    productId: Number;
 
-    constructor(authorName: string, authorId: Number, rating: Number, text: string, expert: boolean) {
+    constructor(authorName: string, authorId: Number, rating: Number, text: string, expert: boolean, productName: string, productId: Number) {
     this.authorName = authorName;
     this.authorId = authorId;
     this.rating = rating;
     this.text = text;
     this.expert = expert;
+    this.productName = productName;
+    this.productId = productId;
     }
 };
