5ee936efad6794b3f2d1d71ffe69822d303fec47
todor-enikov <enigmaenikov@gmail.com>
2016-12-31 14:21:31 +0200

    Moved the logic for logout in the header component file.

--- Liquor_reviews_SPA/app/components/app.main.component.ts
+++ Liquor_reviews_SPA/app/components/app.main.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
-import { AuthenticationService } from '../services/authentication.service';
 
 @Component({
     selector: 'course-app',
@@ -8,18 +6,4 @@ import { AuthenticationService } from '../services/authentication.service';
     styleUrls: ['./app.main.css']
 })
 
-export class AppComponent {
-
-    constructor(private router: Router, private authenticationService: AuthenticationService) { }
-
-    Logout() {
-        this.authenticationService.logout()
-            .then(() => {
-                localStorage.removeItem('loggedUser');
-                this.router.navigate(['']);
-            })
-            .catch((error) => {
-                console.log(error);
-            });
-    }
-}
+export class AppComponent { }
--- Liquor_reviews_SPA/app/components/header.component/header.component.ts
+++ Liquor_reviews_SPA/app/components/header.component/header.component.ts
@@ -1,8 +1,23 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthenticationService } from '../../services/authentication.service';
 
 @Component({
     selector: 'header',
     templateUrl: './header.component.html'
 })
 
-export class HeaderComponent { }
+export class HeaderComponent { 
+    constructor(private router: Router, private authenticationService: AuthenticationService) { }
+
+    Logout() {
+        this.authenticationService.logout()
+            .then(() => {
+                localStorage.removeItem('loggedUser');
+                this.router.navigate(['']);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
+}
