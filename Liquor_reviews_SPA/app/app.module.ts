9244a23cd37c8c44c916364f7c2c66b54d7e6e4e
todor-enikov <enigmaenikov@gmail.com>
2017-01-02 19:24:04 +0200

    Removed unused jquery.

--- Liquor_reviews_SPA/app/app.module.ts
+++ Liquor_reviews_SPA/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule } from '@angular/http';
 import { FormsModule } from '@angular/forms';
-import { } from 'jquery';
 import {
     AboutComponent, AppComponent, WelcomeComponent, LoginComponent, RegisterComponent, UserComponent, UsersComponent,
     ContactComponent, ProductsComponent, ProductComponent, NewProductComponent, ReviewComponent, RatingActiveComponent,
