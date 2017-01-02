50cb1d1c13adcb778f896cc3bb61ae49d0f1c1a1
todor-enikov <enigmaenikov@gmail.com>
2017-01-02 22:30:15 +0200

    Implemented logic for showing product by Id.

--- Liquor_reviews_SPA/app/components/product.components/products.component.ts
+++ Liquor_reviews_SPA/app/components/product.components/products.component.ts
@@ -18,6 +18,7 @@ export class ProductsComponent implements OnInit {
     constructor(private productService: ProductService) { }
 
     ngOnInit() {
-        this.products = this.productService.getAllProducts()
+        this.products = this.productService.getAllProducts();
+        this.products.subscribe();
     }
 }
