5dd6c97a1fedbecbdbcd3592e804644596728f00
todor-enikov <enigmaenikov@gmail.com>
2017-01-03 08:56:29 +0200

    Added property username in model user.

--- Liquor_reviews_SPA/app/models/user.model.ts
+++ Liquor_reviews_SPA/app/models/user.model.ts
@@ -2,13 +2,15 @@ import { Review } from './review.model';
 
 export class User {
     email: string;
-    password: string;
+    password: string;    
+    username: string;
     expert: boolean;
     reviews: Review[];
 
-    constructor(email: string, password: string, expert?: boolean, reviews?: Review[]) {
+    constructor(email: string, password: string,username?: string,  expert?: boolean, reviews?: Review[]) {
         this.email = email;
         this.password = password;
+        this.username = username;        
         this.expert = expert;
         this.reviews = reviews;
     }
