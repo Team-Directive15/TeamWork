1e0a23b1acf72e4e3d7fee34053c9dd2f9d33293
todor-enikov <enigmaenikov@gmail.com>
2017-01-03 09:22:03 +0200

    Finished showing of all users.

--- Liquor_reviews_SPA/app/components/user.components/users.component.ts
+++ Liquor_reviews_SPA/app/components/user.components/users.component.ts
@@ -7,27 +7,17 @@ import { Review } from '../../models/review.model';
 @Component({
     selector: 'users',
     templateUrl: './users.component.html',
-    providers:[UsersService],
+    providers: [UsersService],
     styleUrls: ['./users.component.css']
 })
 
-export class UsersComponent { 
-    
-    users: User[];
-    experts: User[];
+export class UsersComponent {
+    users: FirebaseListObservable<any[]>;
 
     constructor(private userservice: UsersService) { }
 
     ngOnInit() {
-        let allUsers: FirebaseListObservable<User>;
-        // this.userservice.getAllUsers().subscribe(user => {allUsers.push(user)});
-        // allUsers.forEach(user => {
-        //     if (user.expert) {
-        //         this.experts.push(user);
-        //     }
-        //     else {
-        //         this.users.push(user);
-        //     }
-        // });
+        this.users = this.userservice.getAllUsers();
+        this.users.subscribe();
     }
 }
