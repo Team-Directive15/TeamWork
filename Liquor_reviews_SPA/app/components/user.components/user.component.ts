a9eeeb271f1e3c77efc413d26f38d0cfb750fc6c
todor-enikov <enigmaenikov@gmail.com>
2017-01-03 09:43:18 +0200

    Implemented and detail info for user.

--- Liquor_reviews_SPA/app/components/user.components/user.component.ts
+++ Liquor_reviews_SPA/app/components/user.components/user.component.ts
@@ -7,18 +7,35 @@ import { UsersService } from '../../services';
 @Component({
     selector: 'user',
     templateUrl: './user.component.html',
-    providers: [UsersService]
+    providers: [UsersService],
+    styles: [`
+    .specificUser {
+        margin-left: auto;
+        margin-right: auto;
+        margin-top: 20px;
+        margin-bottom: 20px;
+        max-width: 100%;
+        width: auto\9;
+        width: 200px;
+    }
+    `]
 })
 
 export class UserComponent {
-    private _route: ActivatedRoute;
     public user: User;
+    private _route: ActivatedRoute;
+    private _id: string;
 
-    constructor(route: ActivatedRoute) {
+    constructor(route: ActivatedRoute, private usersService: UsersService) {
         this._route = route;
     }
 
     ngOnInit() {
-        // this.user = new User('ExmplUsr', 'aaa', false, [new Review('aa', 2, 3, 'aaa', true, 'prd', 2)]);
+        this._route.params.subscribe(result => this._id = result['id']);
+        this.usersService.getUserByKey(this._id).subscribe(user => this.user = user);
+
+        if (!this.user.reviews) {
+            this.user.reviews = [];
+        }
     }
 }
